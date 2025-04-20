d679ec6b6390c6a81498e2154dd8b315f598e9ac
SreeLakshmi1242 <166686220+SreeLakshmi1242@users.noreply.github.com>
2025-04-20 23:22:24 +0530

    Update chatbot_app_v3_multidialog_with_IndigoRAG_Intent_working.py

--- chatbot_app_v3_multidialog_with_IndigoRAG_Intent_working.py
+++ chatbot_app_v3_multidialog_with_IndigoRAG_Intent_working.py
@@ -6,7 +6,6 @@ import asyncio
 import nest_asyncio
 from transformers import logging, pipeline
 from langchain_community.embeddings import HuggingFaceEmbeddings
-from langchain_community.embeddings import HuggingFaceInferenceAPIEmbeddings
 from langchain_community.embeddings import SpacyEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_community.document_loaders import TextLoader, PyPDFLoader
@@ -41,7 +40,7 @@ nest_asyncio.apply()
 logging.set_verbosity_error()  # Reduce warnings
 
 # Ensure model uses CPU instead of GPU in Streamlit Cloud
-embeddings = HuggingFaceInferenceAPIEmbeddings(
+embeddings = HuggingFaceEmbeddings(
     api_key=st.secrets["auth_key"],model_name="all-MiniLM-L6-v2")
 
 llm = HuggingFaceHub(repo_id="mistralai/Mistral-7B-Instruct-v0.2",
