9a800646f6b82a3bb1e2c86773123612dd154c3b
SreeLakshmi1242 <166686220+SreeLakshmi1242@users.noreply.github.com>
2025-04-22 23:43:30 +0530

    Update chatbot_app_v3_multidialog_with_IndigoRAG_Intent_working.py

--- chatbot_app_v3_multidialog_with_IndigoRAG_Intent_working.py
+++ chatbot_app_v3_multidialog_with_IndigoRAG_Intent_working.py
@@ -25,7 +25,10 @@ faiss_folder = st.sidebar.text_input("FAISS Index Folder Path", value="./faiss_i
 # Initialize embedding model
 @st.cache_resource
 def load_embedding():
-    return HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+    return HuggingFaceEmbeddings(
+    model_name="sentence-transformers/all-MiniLM-L6-v2",
+    model_kwargs={"use_auth_token": hf_token})
+
     
 embedding_function = load_embedding()
 
