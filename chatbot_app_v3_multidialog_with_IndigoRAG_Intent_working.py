fe8c54d769709fea469e17c78621fc2d91db1df6
SreeLakshmi1242 <166686220+SreeLakshmi1242@users.noreply.github.com>
2025-04-25 01:31:21 +0530

    Update chatbot_app_v3_multidialog_with_IndigoRAG_Intent_working.py

--- chatbot_app_v3_multidialog_with_IndigoRAG_Intent_working.py
+++ chatbot_app_v3_multidialog_with_IndigoRAG_Intent_working.py
@@ -127,31 +127,22 @@ def clean_response(response):
     
     return clean_content.strip()
 
+system_instruction = "The assistant should provide detailed explanations."
 # Define the custom prompt template
-prompt_template = PromptTemplate.from_template("""
- You are an Indigo customer assistant. Based on the context - {context}, generate a concise answer for the {question}. For a question on lost baggage, example of answer  as follows -
+template = (
+    f"{system_instruction} "
+    "Combine the chat history and follow up question into "
+    "a standalone question. Chat History: {chat_history}"
+    "Follow up question: {question}"
+)
 
-Thank you for reaching out to Indigo regarding your lost baggage. We sincerely apologize for the inconvenience this may have caused.
-
-To help us locate your baggage as quickly as possible, please provide the following information:
-
-1. Baggage Claim Number (available from the baggage counter or the baggage receipt)
-
-2. Flight Details (Flight Number, Date of Travel, and Departure & Arrival Locations)
-
-3. Description of the Baggage (Color, Size, Type, and any identifying marks)
-
-We are committed to locating your baggage and will provide updates as soon as we have more information. In the meantime, please make sure to check your email and phone for any notifications from our team.
-
-If you have already filed a report with us, you can track the status of your lost baggage by visiting the [Indigo Lost and Found Portal] or contacting our customer service team at [Customer Service Number].
-
-We truly appreciate your patience and understanding. Should you have any additional questions, feel free to reach out, and we’ll be happy to assist you further.
-      
-""")
+# Create the prompt template
+condense_question_prompt = PromptTemplate.from_template(template)
 
 retriever = db.as_retriever()
 memory = ConversationSummaryBufferMemory(llm=llm, memory_key="chat_history", return_messages=True)
-qa_chain = ConversationalRetrievalChain.from_llm(llm=llm, retriever=retriever, memory=memory, combine_docs_chain_kwargs={"prompt": prompt_template})
+qa_chain = ConversationalRetrievalChain.from_llm(llm=llm, retriever=retriever, memory=memory, 
+                                                 condense_question_prompt=condense_question_prompt,chain_type="stuff")
 
 # ----------------------------
 # NLP Pipelines
