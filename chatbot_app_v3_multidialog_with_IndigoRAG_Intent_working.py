b873be1c34064f12acc5156cfd9bf0e21191d823
SreeLakshmi1242 <166686220+SreeLakshmi1242@users.noreply.github.com>
2025-04-25 00:19:27 +0530

    Update chatbot_app_v3_multidialog_with_IndigoRAG_Intent_working.py

--- chatbot_app_v3_multidialog_with_IndigoRAG_Intent_working.py
+++ chatbot_app_v3_multidialog_with_IndigoRAG_Intent_working.py
@@ -8,7 +8,7 @@ from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_huggingface import HuggingFaceEndpoint
 from langchain_community.llms import HuggingFaceHub
-from langchain.memory import ConversationBufferMemory
+from langchain.memory import ConversationSummaryBufferMemory
 
 hf_token = st.secrets["auth_key"]
 # App title
