c1312ec84f2eb26a27d68dc8ad74afca4cf0ec0e
SreeLakshmi1242 <166686220+SreeLakshmi1242@users.noreply.github.com>
2025-04-26 18:45:26 +0530

    Update chatbot_app_v3_multidialog_with_IndigoRAG_Intent_working.py

--- chatbot_app_v3_multidialog_with_IndigoRAG_Intent_working.py
+++ chatbot_app_v3_multidialog_with_IndigoRAG_Intent_working.py
@@ -12,6 +12,12 @@ from langchain.memory import ConversationSummaryBufferMemory
 from langchain.prompts import PromptTemplate
 from langchain.chains import RetrievalQA
 from langchain.schema import Document
+from langchain.schema import(
+AIMessage,
+HumanMessage,
+SystemMessage
+)
+
 
 hf_token = st.secrets["auth_key"]
 # App title
